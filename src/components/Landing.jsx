8f79214de55a1803e12b980fe5f560c4de6df30a
Mati Aragona <matiaragona@gmail.com>
2023-02-21 16:48:57 -0300

    primera preentrega

---
+++ src/components/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import ItemListContainer from './Navbar/ItemListContainer'
+
+
+const landing = () => {
+    
+return (
+<>
+    <Navbar/>
+
+    <ItemListContainer greeting="Bienvenidos A koopstore"/>
+
+</>
+)
+}
+
+export default landing
---
+++ src/components/Navbar/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const itemListContainer = (props) => {
+
+  
+  return (
+    <div className='greeting'>{props.greeting}</div>
+  )
+}
+
+export default itemListContainer
